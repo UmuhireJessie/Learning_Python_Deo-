23e8235aac9f2ca8bc1c9c10c7c686f6f1d6f67a
UmuhireJessie <j.umuhire@alustudent.com>
2021-10-25 14:22:41 +0200

    Added some other lines

--- helloworldpython.py
+++ helloworldpython.py
@@ -1,2 +1,12 @@
-print("\n")
+print('\n')
 print("Hello World!!")
+print('\n')
+print('Did you know that I am coming for you??')
+print('\n')
+print('Yes, you!')
+
+print("--------    -------      -------     ------      |     --------")
+print("   |        |            |           |            |     |")
+print("   |        |______      |______     |______      |     |------")
+print("   |        |                  |           |      |     |")
+print("___|        |______      ______|     ______|      |     |_______")
